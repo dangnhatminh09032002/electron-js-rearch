c47e26ffb34d5599826cc7cfbff46225da901bd4
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-05-12 09:01:29 +0700

    clipboard

--- master-electron/renderer.js
+++ master-electron/renderer.js
@@ -1,20 +1,6 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const { nativeImage, ipcRenderer } = require("electron");
-const path = require("path");
-const fs = require("fs");
+const { clipboard } = require("electron");
 
-const image = nativeImage.createFromPath(
-  path.join(__dirname, "./trayTemplate@5x.png")
-);
-
-ipcRenderer.invoke("get-app-path").then((res) => {
-  const APP_PATH = res;
-  fs.writeFile(
-    path.join(APP_PATH, "image-png.png"),
-    image.getBitmap(),
-    null,
-    () => {}
-  );
-});
+console.log(clipboard.readText());
