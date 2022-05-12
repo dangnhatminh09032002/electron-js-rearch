c766accd06242aaf3e9cad0aa81e642042a65d3e
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-05-12 10:01:39 +0700

    network detection

--- master-electron/main.js
+++ master-electron/main.js
@@ -1,8 +1,5 @@
 // Modules
 const { app, BrowserWindow, ipcMain } = require("electron");
-const path = require("path");
-const fs = require("fs");
-app.disableHardwareAcceleration();
 
 let mainWindow;
 
@@ -17,51 +14,19 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
-    show: true,
     webPreferences: {
       contextIsolation: false,
       nodeIntegration: true,
-      offscreen: true,
     },
   });
 
   // Handle events for webContents
-  mainWindow.webContents.on("did-finish-load", () => {
-    console.log(mainWindow.getTitle());
-    mainWindow.close();
-  });
-
-  const counters = {
-    current: 0,
-  };
-  mainWindow.webContents.on("paint", (event, dirtyRect, image) => {
-    const APP_PATH = app.getAppPath();
-
-    // Check directory exit, if false will create
-    if (counters.current === 0)
-      fs.access(path.join(APP_PATH, "images"), function (error) {
-        if (error) fs.mkdir(path.join(APP_PATH, "images"), () => {});
-      });
-
-    const IMAGES_PATH = path.join(
-      APP_PATH,
-      "images",
-      `image_${counters.current}.png`
-    );
-
-    const screenshot = image.toPNG();
-
-    fs.writeFile(IMAGES_PATH, screenshot, (err, data) => {
-      if (!err) console.log(`image_${counters.current}.png saved`);
-      counters.current++;
-    });
-  });
 
   // Load index.html into the new BrowserWindow
-  mainWindow.loadURL("https://www.facebook.com");
+  mainWindow.loadFile("./index.html");
 
   // Open DevTools - Remove for PRODUCTION!
-  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 
   // Listen for window being closed
   mainWindow.on("closed", () => {
--- master-electron/renderer.js
+++ master-electron/renderer.js
@@ -1,3 +1,18 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const updateStatus = () => {
+  const statusElement = document.getElementById("status");
+  statusElement.innerHTML = navigator.onLine ? "Online" : "Offline";
+};
+
+window.addEventListener("load", () => {
+  updateStatus();
+  window.removeEventListener("load", () => {});
+  window.addEventListener("online", () => {
+    updateStatus();
+  });
+  window.addEventListener("offline", () => {
+    updateStatus();
+  });
+});
