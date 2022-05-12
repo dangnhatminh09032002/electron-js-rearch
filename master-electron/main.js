89fa7c3cf8b04290181e35509e14ce47e375c16c
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-05-12 09:38:45 +0700

    offcreen rendering

--- master-electron/main.js
+++ master-electron/main.js
@@ -1,5 +1,9 @@
 // Modules
 const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const fs = require("fs");
+app.disableHardwareAcceleration();
+
 let mainWindow;
 
 const loggerInBrowser = (details) => {
@@ -13,20 +17,51 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
+    show: true,
     webPreferences: {
-      // --- !! IMPORTANT !! ---
-      // Disable 'contextIsolation' to allow 'nodeIntegration'
-      // 'contextIsolation' defaults to "true" as from Electron v12
       contextIsolation: false,
       nodeIntegration: true,
+      offscreen: true,
     },
   });
 
+  // Handle events for webContents
+  mainWindow.webContents.on("did-finish-load", () => {
+    console.log(mainWindow.getTitle());
+    mainWindow.close();
+  });
+
+  const counters = {
+    current: 0,
+  };
+  mainWindow.webContents.on("paint", (event, dirtyRect, image) => {
+    const APP_PATH = app.getAppPath();
+
+    // Check directory exit, if false will create
+    if (counters.current === 0)
+      fs.access(path.join(APP_PATH, "images"), function (error) {
+        if (error) fs.mkdir(path.join(APP_PATH, "images"), () => {});
+      });
+
+    const IMAGES_PATH = path.join(
+      APP_PATH,
+      "images",
+      `image_${counters.current}.png`
+    );
+
+    const screenshot = image.toPNG();
+
+    fs.writeFile(IMAGES_PATH, screenshot, (err, data) => {
+      if (!err) console.log(`image_${counters.current}.png saved`);
+      counters.current++;
+    });
+  });
+
   // Load index.html into the new BrowserWindow
-  mainWindow.loadFile("index.html");
+  mainWindow.loadURL("https://www.facebook.com");
 
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Listen for window being closed
   mainWindow.on("closed", () => {
